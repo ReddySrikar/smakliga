999ee47c20021262db305d0ac49e895f8efc5760
Srikar Reddy Nadikattu <srikar.nr@gmail.com>
2018-08-05 11:01:10 +0200

    Add babel dependenicies
    Added Babel modules: cre, loader and preset-env
    babel-loader is the webpack plugin for Babel
    babel-preset-env is for setting targets
    and environments

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: ['babel-polyfill','./src/js/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.js'
@@ -15,7 +15,18 @@ module.exports = {
       filename: 'index.html',
       template: './src/index.html'
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 };
 // The path property can only take an absolute path as an attribute, so we cannot write the whole path, and instead we use...
 // Here, "path" is an inbuilt npm package that needs to be used here to get the absolute path.
